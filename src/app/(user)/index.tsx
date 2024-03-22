958dec15d6c04411c92962d9e825aa9ef4e303c3
benbugraer <bugraerdev@gmail.com>
2024-03-23 00:02:29 +0300

    Orders Page Created

--- src/app/(user)/index.tsx
+++ src/app/(user)/index.tsx
@@ -1,5 +1,9 @@
 import { Redirect } from "expo-router";
 
 export default function TabIndex() {
-  return <Redirect href={"/(user)/menu/"} />;
+  return (
+    <>
+      <Redirect href={"/(user)/menu/"} />
+    </>
+  );
 }
