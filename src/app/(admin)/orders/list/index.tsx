6c102b8c33fe5f227409bd45168e1a38141fd1dd
benbugraer <bugraerdev@gmail.com>
2024-03-30 16:12:28 +0300

    New Orders Subscription

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -4,7 +4,8 @@ import { ActivityIndicator, FlatList, Text } from "react-native";
 import OrderListItem from "@/components/OrderListItem";
 import { useAdminOrderList } from "@/api/orders";
 import { supabase } from "@/lib/supabase";
-import orders from "@assets/data/orders";
+import { useQueryClient } from "@tanstack/react-query";
+import { useInsertOrderSubscription } from "@/api/orders/subscription";
 
 export default function OrdersScreen() {
   const {
@@ -12,6 +13,8 @@ export default function OrdersScreen() {
     isLoading,
     error,
   } = useAdminOrderList({ archived: false });
+
+  useInsertOrderSubscription();
 
   if (isLoading) {
     return <ActivityIndicator />;
@@ -21,22 +24,6 @@ export default function OrdersScreen() {
     return <Text>Failed to fetch orders.</Text>;
   }
 
-  useEffect(() => {
-    const orders = supabase
-      .channel("custom-insert-channel")
-      .on(
-        "postgres_changes",
-        { event: "INSERT", schema: "public", table: "orders" },
-        (payload) => {
-          console.log("Change received!", payload);
-          refetch();
-        }
-      )
-      .subscribe();
-    return () => {
-      orders.unsubscribe();
-    };
-  }, []);
   return (
     <>
       <FlatList
@@ -46,4 +33,7 @@ export default function OrdersScreen() {
       />
     </>
   );
+}
+function subscribe() {
+  throw new Error("Function not implemented.");
 }
